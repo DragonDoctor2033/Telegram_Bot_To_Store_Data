b280f2ec8e7d1aa502d617f3dfbd44d02d0092bf
Dragon_Doctor <doctor2_33@protonmail.com>
2021-07-09 08:29:57 +0300

    Bot store data from telegram and save in EXCEL file

---
+++ Store_File_and_Send.py
@@ -0,0 +1,72 @@
+from datetime import date
+
+import pandas as pd
+from openpyxl import load_workbook
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.lib import colors
+counter = 0
+
+
+def save_as_pdf(user_data: dict, index: str) -> str:
+    file_name = index + "_" + user_data['Имя клиента'] + '.pdf'
+    document_title = 'Ремонт хуй знает где'
+    title = 'Пока хуй знает, где и что будет показывать.'
+    text_lines = [
+        'Name: ' + user_data['Имя клиента'],
+        'Phone Number: ' + user_data['Номер телефона'],
+        'Issue: ' + user_data['Поломка']
+    ]
+    pdf = canvas.Canvas(file_name)
+    pdf.setTitle(document_title)
+    pdfmetrics.registerFont(TTFont('abc', 'VeraBI.ttf'))
+    pdf.setFont('abc', 36)
+    pdf.drawCentredString(300, 700, title)
+    pdf.setFillColorRGB(0, 0, 255)
+    pdf.setFont("Courier-Bold", 24)
+    pdf.drawCentredString(290, 720, 'А я ебу, что тут писать? ')
+    pdf.line(30, 710, 550, 710)
+    text = pdf.beginText(40, 680)
+    text.setFont("Courier", 18)
+    text.setFillColor(colors.red)
+    for line in text_lines:
+        text.textLine(line)
+    pdf.drawText(text)
+    pdf.save()
+    return file_name
+
+
+def assigned_data_to_excel(file_name: str, user_data: dict, repair_number: str, row: int) -> None:
+    """
+    Задаём значения в строки excel таблицы по номеру последней строки + 1
+    """
+    wb = load_workbook(file_name)
+    sheet = wb.active
+    sheet["A" + str(row + 1)] = repair_number
+    sheet["B" + str(row + 1)] = user_data['Имя клиента']
+    sheet["C" + str(row + 1)] = user_data['Номер телефона']
+    sheet["D" + str(row + 1)] = user_data['Поломка']
+    wb.save('Customers Data Base.xlsx')
+
+
+def store_file(user_data) -> str:
+    global counter
+    counter += 1
+    today = date.today()
+    wb = load_workbook('Customers Data Base.xlsx')
+    sheet = wb.active
+    last_row = sheet.max_row
+    last_row_value = sheet.cell(column=1, row=last_row).value
+    if today.strftime('%d.%m.%y') != last_row_value[:8]:
+        counter = 1  # Если дата сменилась, то обнуляем счётчик
+    elif int(last_row_value[9:]) > counter:
+        counter = int(last_row_value[9:]) + 1  # Если номер ремонта того же дня больше, то увеличиваем счётчик
+    index = today.strftime('%d.%m.%y.') + str(counter).zfill(2)  # Создаём номер ремонта типа "dd.mm.yy.client number"
+    assigned_data_to_excel(file_name='Customers Data Base.xlsx', user_data=user_data, repair_number=index, row=last_row)
+    return str(index) + save_as_pdf(user_data=user_data, index=index)
+
+
+df = pd.read_excel("Customers Data Base.xlsx")
+df_abd = df[df == '08.07.21.01']
+print(df_abd)
---
+++ main.py
@@ -0,0 +1,143 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters, \
+    ConversationHandler
+from Token_BOT_SQL import Token
+import logging
+from requests import post
+from Store_File_and_Send import store_file
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+GET, DATA, PHONE_NUMBER, ISSUE, WHICH_ONE, CORRECT_INFO = range(6)
+
+
+def facts_to_str(user_data) -> str:
+    facts = [f'{key} - {value}' for key, value in user_data.items()][1:]
+    return "\n".join(facts).join(['\n', '\n'])
+
+
+def start(update: Update, context: CallbackContext) -> int:
+    keyboard = [
+        [
+            InlineKeyboardButton('Приём.', callback_data='Get_Device'),
+            # InlineKeyboardButton('Выдача.', callback_data='Return_Device')
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Что хочешь сделать?', reply_markup=reply_markup)
+    return GET
+
+
+def customer_name(update: Update, context: CallbackContext) -> int:
+    context.user_data['Имя клиента'] = update.message.text
+    update.message.reply_text('Номер телефона?')
+    return PHONE_NUMBER
+
+
+def phone_number(update: Update, context: CallbackContext) -> int:
+    context.user_data['Номер телефона'] = update.message.text
+    update.message.reply_text("Что случилось?")
+    return ISSUE
+
+
+def mistake_was_made(update: Update, context: CallbackContext) -> int:
+    keyboard = [
+            ['Имя клиента', 'Номер телефона', 'Поломка']
+    ]
+    markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    update.message.reply_text(text='Где ошибка?', reply_markup=markup)
+    return WHICH_ONE
+
+
+def what_happened(update: Update, context: CallbackContext) -> None:
+    context.user_data['Поломка'] = update.message.text
+    update.message.reply_text(f'Убедись, что написал всё верно. \n{facts_to_str(context.user_data)}'
+                              f'Если да, жми /save. \nЕсли ошибся, то /mistake.')
+    return ConversationHandler.END
+
+
+def save_order(update: Update, context: CallbackContext) -> None:
+    text = store_file(context.user_data)
+    update.message.reply_text(text=text[:11])
+    message = f'https://api.telegram.org/bot{Token}/sendDocument?chat_id={context.user_data["chat_id"]}'
+    post(message, files={'document': open(text[11:], 'rb')})
+    context.user_data.clear()
+
+
+def button(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    print(query)
+    if '470529631' in str(query):
+        context.user_data['chat_id'] = '470529631'
+    elif '669528071' in str(query):
+        context.user_data['chat_id'] = '669528071'
+    else:
+        return ConversationHandler.END
+    if query['data'] == 'Get_Device':
+        query.edit_message_text('Как зовут?')
+        return GET
+    # if query['data'] == 'Return_Device':
+    #     query.edit_message_text('Номер ремонта:')
+
+
+def category_mistake(update: Update, context: CallbackContext) -> int:
+    global category
+    category = update.message.text
+    update.message.reply_text(f'Понял. Ошибка в {category}. Можешь исправить.')
+    return CORRECT_INFO
+
+
+def correction_info(update: Update, context: CallbackContext) -> int:
+    context.user_data[category] = update.message.text
+    update.message.reply_text(f'Убедись, что написал всё верно. \n{facts_to_str(context.user_data)}'
+                              f'Если да, жми /save. \nЕсли ошибся, то  жми /mistake.')
+    return ConversationHandler.END
+
+
+def main(user_limit: list) -> None:
+    updater = Updater(Token)
+    add = updater.dispatcher.add_handler
+    gather_info_customer = ConversationHandler(
+        entry_points=[CommandHandler('start', start, Filters.user(user_limit))],
+        states={
+            GET: [
+                MessageHandler(Filters.text, customer_name),
+            ],
+            DATA: [
+                add(CallbackQueryHandler(button))
+            ],
+            PHONE_NUMBER: [
+                MessageHandler(Filters.text, phone_number)
+            ],
+            ISSUE: [
+                MessageHandler(Filters.text, what_happened)
+            ],
+        },
+        fallbacks=[CommandHandler('start', start, Filters.user(user_limit))]
+    )
+    mistake_handler = ConversationHandler(
+        entry_points=[CommandHandler('mistake', mistake_was_made, Filters.user(user_limit))],
+        states={
+            WHICH_ONE: [
+                MessageHandler(Filters.regex('^(Имя клиента|Номер телефона|Поломка)$'), category_mistake)
+            ],
+            CORRECT_INFO: [
+                MessageHandler(Filters.text & ~Filters.command, correction_info)
+            ]
+        },
+        fallbacks=[CommandHandler('mistake', mistake_was_made, Filters.user(user_limit))]
+    )
+    add(CommandHandler('save', save_order, Filters.user(user_limit)))
+    add(gather_info_customer)
+    add(mistake_handler)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    user_list = [669528071, 470529631]
+    main(user_list)
